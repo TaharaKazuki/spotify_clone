14078ee4cfbaf753c08d5a45a5892a37a4b99d6b
田原 一樹 <ssp0727.lnc@gmail.com>
2019-12-03 22:07:16 +0900

    [update] add login function

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -13,5 +13,6 @@ class Constants {
 	public static $usernameCharacters = "Your username must be between 5 and 25 characters";
 	public static $usernameTaken = "This username already exists";
 
+	public static $loginFailed = "Your username or password was incorrect";
 }
 ?>
